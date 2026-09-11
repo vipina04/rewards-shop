b6f7ae185fdaa1b9ff4f655a7f4d9fbc28fdeac1
vipina04 <97424919+vipina04@users.noreply.github.com>
2026-09-11 06:45:49 +0000

    add homepage test

---
+++ tests/test_app.py
@@ -0,0 +1,20 @@
+import pytest
+
+from app import create_app
+
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config["TESTING"] = True
+    return app
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+def test_homepage(client):
+    response = client.get("/")
+    assert response.status_code == 200
